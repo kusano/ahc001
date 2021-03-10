f493e4f8bb4582ed1f12c3a3c7fe46f3e947158e
kusano <kusano@users.noreply.github.com>
2021-03-11 05:25:51 +0900

    広げられるだけ広げる
     0: 1.069  901877674
     1: 0.518  905167714
     2: 0.350  856993066
     3: 0.605  893272804
     4: 0.564  906964988
     5: 0.544  895587328
     6: 0.606  862733435
     7: 0.326  902178825
     8: 0.531  873302764
     9: 0.350  929164549
    sum: 8927243147

--- ahc001.cpp
+++ ahc001.cpp
@@ -4,8 +4,12 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 #include <cstdio>
 using namespace std;
+
+const int W = 10000;
+const int H = 10000;
 
 int score(int n, vector<int> x, vector<int> y, vector<int> r,
     vector<int> a, vector<int> b, vector<int> c, vector<int> d)
@@ -25,19 +29,65 @@ int score(int n, vector<int> x, vector<int> y, vector<int> r,
     return int(1e9*p/n+.5);
 }
 
+int xor64(void) {
+    static uint64_t x = 88172645463325252ULL;
+    x ^= x<<13;
+    x ^= x>> 7;
+    x ^= x<<17;
+    return int(x&0x7fffffff);
+}
+
 vector<vector<int>> solve(int n, vector<int> x, vector<int> y, vector<int> r)
 {
-    vector<int> a(n), b(n), c(n), d(n);
+    vector<int> x1(n), y1(n), x2(n), y2(n);
 
     for (int i=0; i<n; i++)
     {
-        a[i] = x[i];
-        b[i] = y[i];
-        c[i] = x[i]+1;
-        d[i] = y[i]+1;
+        x1[i] = x[i];
+        y1[i] = y[i];
+        x2[i] = x[i]+1;
+        y2[i] = y[i]+1;
     }
 
-    return {a, b, c, d};
+    auto area = [&](int p)
+    {
+        return (x2[p]-x1[p])*(y2[p]-y1[p]);
+    };
+
+    int dx1[] = {-1, 0, 0, 0};
+    int dy1[] = {0, -1, 0, 0};
+    int dx2[] = {0, 0, 1, 0};
+    int dy2[] = {0, 0, 0, 1};
+
+    for (int iter=0; iter<10'000'000; iter++)
+    {
+        int p = xor64()%n;
+        if (area(p)<r[p])
+        {
+            int d = xor64()%4;
+            bool ok = true;
+            if (x1[p]+dx1[d]<0 || W<x2[p]+dx2[d] ||
+                y1[p]+dy1[d]<0 || H<y2[p]+dy2[d])
+                ok = false;
+
+            for (int i=0; i<n && ok; i++)
+                if (i!=p)
+                    if (!(x2[i]<=x1[p]+dx1[d] ||
+                          x1[i]>=x2[p]+dx2[d] ||
+                          y2[i]<=y1[p]+dy1[d] ||
+                          y1[i]>=y2[p]+dy2[d]))
+                        ok = false;
+            if (ok)
+            {
+                x1[p] += dx1[d];
+                y1[p] += dy1[d];
+                x2[p] += dx2[d];
+                y2[p] += dy2[d];
+            }
+        }
+    }
+
+    return {x1, y1, x2, y2};
 }
 
 void evaluate()
@@ -55,7 +105,10 @@ void evaluate()
         for (int j=0; j<n; j++)
             in>>x[j]>>y[j]>>r[j];
 
+        chrono::system_clock::time_point start = chrono::system_clock::now();
         vector<vector<int>> t = solve(n, x, y, r);
+        chrono::system_clock::time_point end = chrono::system_clock::now();
+
         vector<int> a = t[0];
         vector<int> b = t[1];
         vector<int> c = t[2];
@@ -65,8 +118,9 @@ void evaluate()
         for (int j=0; j<n; j++)
             out<<a[j]<<" "<<b[j]<<" "<<c[j]<<" "<<d[j]<<endl;
 
+        long long tm = chrono::duration_cast<chrono::milliseconds>(end-start).count();
         int s = score(n, x, y, r, a, b, c, d);
-        printf("%2d: %10d\n", i, s);
+        printf("%2d: %5.3f %10d\n", i, tm*1e-3, s);
 
         sum += s;
     }
